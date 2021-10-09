c2fd091afe6f2b85ed77b634e59302f088239203
pasan1 <pasan.lahiru123@gmail.com>
2021-10-09 09:51:55 +0530

    Added AppInitializer.java

---
+++ src/AppInitializer.java
@@ -0,0 +1,26 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *  *  Copyright (c) Speed Way. All rights reserved.
+ *  *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *  *--------------------------------------------------------------------------------------------
+ */
+
+/**
+ * @author Pasan Abeysekara <pasan.lahiru123@gmail.com> (www.pasanabeysekara.com)
+ * @since 10/9/2021
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
